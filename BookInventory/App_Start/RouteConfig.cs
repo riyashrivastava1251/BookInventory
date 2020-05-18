144c89bf8768d102fdc05bc47b3b8508e76f004e
Riya Shrivastava <riyashrivastava1251@gmail.com>
2020-05-18 21:01:59 +0530

    book inventory managemnet  system

--- BookInventory/App_Start/RouteConfig.cs
+++ BookInventory/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookInventory
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Books", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
---
+++ BookInventory/Controllers/BooksController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookInventory;
+
+namespace BookInventory.Controllers
+{
+    public class BooksController : Controller
+    {
+        private inventoryDBEntities db = new inventoryDBEntities();
+
+        // GET: Books
+        public ActionResult Index(string Search = null)
+        {
+            int isnb;
+            if (Search != null)
+            {
+                bool success = Int32.TryParse(Search, out isnb);
+                if (!success)
+                {
+                    isnb = 0;
+                }
+                var foundBooks = (db.Books.Where(e => e.Book_Title.Contains(Search) || e.Author.Contains(Search) || e.ISBN_Code.Equals(isnb))).ToList();
+                return View(foundBooks);
+                
+                
+            }
+            return View(db.Books.ToList());
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Books/Create
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ISBN_Code,Book_Title,Author,Publication,Publication_year")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ISBN_Code,Book_Title,Author,Publication,Publication_year")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
